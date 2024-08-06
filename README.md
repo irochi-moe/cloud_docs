bfd6b44d948c6370632f48aa536b72a636bef4f5
Irochi <me@irochi.moe>
2024-08-06 17:03:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Irochi's Cloud 문서
+환영합니다! Irochi's Cloud 문서 저장소입니다.
+
+본 저장소는 Node.js 와 pnpm 을 사용합니다. 아래 명령어를 사용해 직접 테스트해볼 수 있습니다.
+
+```
+pnpm install
+pnpm start
+```
+
+For the love of, [Docusaurus 3](https://github.com/facebook/docusaurus)
---
+++ docusaurus.config.ts
@@ -0,0 +1,76 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+
+const config: Config = {
+  title: 'Irochi\'s Cloud 문서',
+  tagline: 'Presented by irochi.moe',
+  url: 'https://docs.cloud.irochi.moe/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  trailingSlash: false,
+  organizationName: 'cloud_docs',
+  projectName: 'cloud_docs',
+
+  i18n: {
+    defaultLocale: 'ko',
+    locales: ['ko'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          path: 'src/docs',
+          routeBasePath: '/',
+        },
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      // title: 'Irochi\'s Cloud 문서',
+      logo: {
+        alt: 'Irochi\'s Cloud',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          href: 'https://discord.cloud.irochi.moe',
+          label: '디스코드',
+          position: 'left',
+        },
+        /* {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Irochi\'s Cloud 문서',
+        }, */
+      ],
+    },
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+      respectPrefersColorScheme: true,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
+};
+
+export default config;
